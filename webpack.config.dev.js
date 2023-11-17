af9be7d9b8bf536f244a731fad0ab02631a7b931
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2023-11-17 19:28:44 +0000

    Fix indentention size and defer js load

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,12 +3,12 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
-  devServer: {
-    liveReload: true,
-    hot: true,
-    open: true,
-    static: ["./"],
-  },
+	mode: "development",
+	devtool: "inline-source-map",
+	devServer: {
+		liveReload: true,
+		hot: true,
+		open: true,
+		static: ["./"],
+	},
 });
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,23 +5,22 @@ const CopyPlugin = require("copy-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "production",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: "img", to: "img" },
-        { from: "css", to: "css" },
-        { from: "js/vendor", to: "js/vendor" },
-        { from: "icon.svg", to: "icon.svg" },
-        { from: "favicon.ico", to: "favicon.ico" },
-        { from: "robots.txt", to: "robots.txt" },
-        { from: "icon.png", to: "icon.png" },
-        { from: "404.html", to: "404.html" },
-        { from: "site.webmanifest", to: "site.webmanifest" },
-      ],
-    }),
-  ],
+	mode: "production",
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./index.html",
+		}),
+		new CopyPlugin({
+			patterns: [
+				{ from: "img", to: "img" },
+				{ from: "css", to: "css" },
+				{ from: "icon.svg", to: "icon.svg" },
+				{ from: "favicon.ico", to: "favicon.ico" },
+				{ from: "robots.txt", to: "robots.txt" },
+				{ from: "icon.png", to: "icon.png" },
+				{ from: "404.html", to: "404.html" },
+				{ from: "site.webmanifest", to: "site.webmanifest" },
+			],
+		}),
+	],
 });
